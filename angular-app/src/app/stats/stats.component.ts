02a2fd803be680df8ecad7e728d24eba546e5c6f
Antonio Ordóñez <aom387@inlumine.ual.es>
2025-03-25 19:01:57 +0100

    Add profile images and update sidebar styles for consistency

--- angular-app/src/app/stats/stats.component.ts
+++ angular-app/src/app/stats/stats.component.ts
@@ -1,29 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-stats',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './stats.component.html',
   styleUrl: './stats.component.css'
 })
 export class StatsComponent {
-  constructor(private router: Router) {}
+  //constructor(private router: Router) {}
 
+  
+
+  ngOnInit(): void{
+    console.log("Hola");
+  }
   goToHome(event: Event) {
     event.preventDefault();
     console.log('Navegando a Home');
-    this.router.navigate(['/home']);
+    //this.router.navigate(['/home']);
   }
 
   goToStats() {
     console.log('Navegando a Stats');
-    this.router.navigate(['/stats']);
+    //this.router.navigate(['/stats']);
   }
 
-  goToSettings() {
+  pepe(){
     console.log('Navegando a Settings');
-    this.router.navigate(['/settings']);
+    //this.router.navigate(['/settings']);
   }
 }
