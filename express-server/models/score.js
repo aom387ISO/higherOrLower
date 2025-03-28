ee142f3c773910a69f756f8feecb7f9dd9423feb
Antonio Ordóñez <aom387@inlumine.ual.es>
2025-03-28 22:46:39 +0100

    Fixing name

--- express-server/models/score.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ScoreSchema = new mongoose.Schema({
-    bestMangaScore: { type: Number, required: true },
-    bestAnimeScore: { type: Number, required: true },
-    idUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
-});
-
-module.exports = mongoose.model('Score', ScoreSchema);
